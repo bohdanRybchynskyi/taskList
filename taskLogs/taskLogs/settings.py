5fa6f10f57c715deedd032e1479ce6c638590de0
bohdan <b.rybchynskyi@infinitytechnologies.ch>
2024-03-13 14:03:10 +0200

    project setup

--- taskLogs/taskLogs/settings.py
+++ taskLogs/taskLogs/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'taskLogs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+           'ENGINE': 'djongo',
+           'NAME': 'logs',
+       }
 }
 
 
--- taskManager/taskManager/settings.py
+++ taskManager/taskManager/settings.py
@@ -74,9 +74,12 @@ WSGI_APPLICATION = 'taskManager.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "task_service",
+            "passfile": ".pgpass",
+        },
     }
 }
 
